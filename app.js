1c58ec04101adf1d5ed8955888287dc1ff57a9e7
zero_jeden <zeroitjeden@gmail.com>
2024-08-11 19:05:49 +0200

    feat: nnew functionalities enabling the collection of data from the user and calculation.

---
+++ app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+function showResult(result) {
+	alert(`Result is: ${result}`);
+}
+function getNumberFromUser() {
+	var input = prompt('Add number:', 'only number');
+	return Number(input);
+}
+function getOperatorFromUser() {
+	return prompt('Add operator: +, -, *, /');
+}
+function getConfirmationFromUser() {
+	return confirm('Do you want to continue?');
+}
+function getConfirmationFromUserAndContinue(result) {
+	var confirmation = getConfirmationFromUser();
+
+	if (confirmation) {
+		mainLoop(result);
+	} else {
+		showResult(result);
+	}
+}
+
+function calculate(operator, x, y) {
+	switch (operator) {
+		case '+':
+			return x + y;
+		case '-':
+			return x - y;
+		case '*':
+			return x * y;
+		case '/':
+			return x / y;
+	}
+}
+
+function mainLoop(result) {
+	var operator = getOperatorFromUser();
+	var number = getNumberFromUser();
+	var newResult = calculate(operator, result, number);
+
+	getConfirmationFromUserAndContinue(newResult);
+}
+
+var result = getNumberFromUser();
+getConfirmationFromUserAndContinue(result);
