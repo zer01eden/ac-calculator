b91bcacade4b31389c07bd2ad1c7adb12b68efd7
zero_jeden <zeroitjeden@gmail.com>
2024-08-12 21:55:45 +0200

    fix: hidden variables using closure

--- app.js
+++ app.js
@@ -1,68 +1,71 @@
 'use strict';
 
-function showResult(result) {
-	alert(`Result is: ${result}`);
-}
-function getNumberFromUser() {
-	var input = prompt('Add number:', 'only number');
-	var number = Number(input);
-
-	if (Number.isNaN(number)) {
-		return getNumberFromUser();
-	} else {
-		return number;
+var startCalculator = (function () {
+	function showResult(result) {
+		alert(`Result is: ${result}`);
 	}
-}
-function getOperatorFromUser() {
-	var operator = prompt('Add operator: +, -, *, /');
-	switch (operator) {
-		case '+':
-			return operator;
-		case '-':
-			return operator;
-		case '*':
-			return operator;
-		case '/':
-			return operator;
-		default:
-			return getOperatorFromUser();
+	function getNumberFromUser() {
+		var input = prompt('Add number:', 'only number');
+		var number = Number(input);
+
+		if (Number.isNaN(number)) {
+			return getNumberFromUser();
+		} else {
+			return number;
+		}
 	}
-}
-function getConfirmationFromUser() {
-	return confirm('Do you want to continue?');
-}
-function getConfirmationFromUserAndContinue(result) {
-	var confirmation = getConfirmationFromUser();
-
-	if (confirmation) {
-		mainLoop(result);
-	} else {
-		showResult(result);
+	function getOperatorFromUser() {
+		var operator = prompt('Add operator: +, -, *, /');
+		switch (operator) {
+			case '+':
+				return operator;
+			case '-':
+				return operator;
+			case '*':
+				return operator;
+			case '/':
+				return operator;
+			default:
+				return getOperatorFromUser();
+		}
 	}
-}
-
-function calculate(operator, x, y) {
-	switch (operator) {
-		case '+':
-			return x + y;
-		case '-':
-			return x - y;
-		case '*':
-			return x * y;
-		case '/':
-			return x / y;
+	function getConfirmationFromUser() {
+		return confirm('Do you want to continue?');
 	}
-}
+	function getConfirmationFromUserAndContinue(result) {
+		var confirmation = getConfirmationFromUser();
 
-function mainLoop(result) {
-	var operator = getOperatorFromUser();
-	var number = getNumberFromUser();
-	var newResult = calculate(operator, result, number);
+		if (confirmation) {
+			mainLoop(result);
+		} else {
+			showResult(result);
+		}
+	}
 
-	getConfirmationFromUserAndContinue(newResult);
-}
+	function calculate(operator, x, y) {
+		switch (operator) {
+			case '+':
+				return x + y;
+			case '-':
+				return x - y;
+			case '*':
+				return x * y;
+			case '/':
+				return x / y;
+		}
+	}
 
-function start() {
-	var result = getNumberFromUser();
-	getConfirmationFromUserAndContinue(result);
-}
+	function mainLoop(result) {
+		var operator = getOperatorFromUser();
+		var number = getNumberFromUser();
+		var newResult = calculate(operator, result, number);
+
+		getConfirmationFromUserAndContinue(newResult);
+	}
+
+	function start() {
+		var result = getNumberFromUser();
+		getConfirmationFromUserAndContinue(result);
+	}
+	return start;
+})();
